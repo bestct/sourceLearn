a6dfb42c727d8485c4eb9f600268e8e2aad7ab44
bestct <977961764@qq.com>
2021-03-25 13:29:53 +0800

    Initial commit

---
+++ src/SourceMain.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+/**
+ * @author :长亭残木
+ * @PackageName : com.ct
+ * @ClassName :  SourceMain
+ * @Description :
+ * @date :2021/3/25 11:56
+ */
+
+
+public class SourceMain {
+
+    public static void main(String[] args) {
+        HashMap<String,Integer> map=new HashMap<>(9);
+        map.put("k1",1);
+        map.put("k2",2);
+        map.put("k3",3);
+        map.put("k4",4);
+        System.out.println();
+    }
+}
---
+++ src/test/MapTest.java
@@ -0,0 +1,20 @@
+package com.ct.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author :长亭残木
+ * @PackageName : com.ct.test
+ * @ClassName :  MapTest
+ * @Description :
+ * @date :2021/3/25 12:04
+ */
+
+
+public class MapTest {
+
+    public static void main(String[] args) {
+        Map<String,Integer> map=new HashMap<>();
+    }
+}
